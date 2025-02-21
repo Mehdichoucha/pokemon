d6e08fcd4aa09d805e280e705835a52e8eaffb07
Linda Choucha <mehdi.choucha@laplateforme.io>
2025-02-21 12:59:32 +0100

    :sparkles: the fight system and the main menu are working

--- battle.py
+++ battle.py
@@ -41,30 +41,51 @@ class Battle:
     
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+SCREEN = pygame.display.set_mode((800, 600))
 WHITE =(255,255,255)
 BLACK =(0,0,0)
 GREEN = (0,255,0)
 RED = (255,0,0)
 FONT = pygame.font.Font(None,20)
 WIDTH,HEIGHT = 800,600
+BACKGROUND = pygame.transform.scale(pygame.image.load("images/battle_assets/background.png"), (800, 600))
 
-circle1_x, circle1_y = 100, 400
-circle2_x, circle2_y = 400, 500
-circle_radius = 50
 
+
+menu = pygame.transform.scale(pygame.image.load("images/menu/menu2.png"), (800, 200))
+button = pygame.image.load("images/menu/button.png")
+hp1 = pygame.transform.scale(pygame.image.load("images/battle_assets/hp3.png"), (270, 80))
+hp2 = pygame.transform.scale(pygame.image.load("images/battle_assets/hp4.png"), (270, 80))
+bush1 = pygame.transform.scale(pygame.image.load("images/battle_assets/bush1.png"), (370, 100))
+bush2 = pygame.transform.scale(pygame.image.load("images/battle_assets/bush2.png"), (600, 90))
+background = pygame.image.load("images/battle_assets/background.png")
 
 pokemon1 = Pokemon("Pikachu",100,1,55,40,"electric",0)
+main_pokemon = pygame.transform.scale(pygame.image.load("images/pokemon/pikachu2.png"), (190, 250))
+c = 30
+d = 190
 
 random_wild = random.randint(1, 4)
 if random_wild == 1:
     pokemon4 = Pokemon("Mustébouée",100,1,65,35,"water",0)
+    wild_pokemon = pygame.transform.scale(pygame.image.load("images/pokemon/buizel1.png"), (126, 150))
+    a = 550
+    b = 65
 if random_wild == 2:
     pokemon4 = Pokemon("Salamèche",39,1,52,43,"fire",0)
+    wild_pokemon = pygame.transform.scale(pygame.image.load("images/pokemon/charmender1.png"), (126, 150))
+    a = 550
+    b = 65
 if random_wild == 3:
     pokemon4 = Pokemon("Bulbizarre", 45, 1,49, 49, "plant",0)
+    wild_pokemon = pygame.transform.scale(pygame.image.load("images/pokemon/bulbasaur1.png"), (126, 150))
+    a = 550
+    b = 65
 if random_wild == 4:
     pokemon4 = Pokemon("Pikachu",100,1,55,40,"electric",0)
+    wild_pokemon = pygame.transform.scale(pygame.image.load("images/pokemon/pikachu.png"), (126, 150))
+    a = 550
+    b = 65
 
 
 
@@ -80,15 +101,15 @@ def draw_action_button():
     button_margin = 20
     buttons = [
         {"text": "Attack","rect": pygame.Rect(button_margin, HEIGHT - button_height - button_margin,button_width, button_height)},
-        {"text": "Change", "rect": pygame.Rect(button_margin * 2 + button_width, HEIGHT - button_height - button_margin,
+        {"text": "Catch", "rect": pygame.Rect(button_margin * 2 + button_width, HEIGHT - button_height - button_margin,
         button_width,button_height)},
         {"text": "Escape", "rect": pygame.Rect(button_margin * 3 + button_width * 2, HEIGHT - button_height- button_margin,
         button_width, button_height)},
     ]
     for button in buttons:
-        pygame.draw.rect(screen,GREEN, button["rect"])
+        pygame.draw.rect(SCREEN,GREEN, button["rect"])
         text_surface = FONT.render(button["text"],True,BLACK)
-        screen.blit(text_surface, (button["rect"].x + 10,button["rect"].y + 10))
+        SCREEN.blit(text_surface, (button["rect"].x + 10,button["rect"].y + 10))
     return buttons 
 
 
@@ -99,27 +120,27 @@ def draw_attack_message(message):
     text_surface = FONT.render(message, True, BLACK)
     text_rect = text_surface.get_rect(topleft=(10, 10))  
     message_surface.blit(text_surface, text_rect)
-    screen.blit(message_surface, (WIDTH - 330, HEIGHT - 60))  
+    SCREEN.blit(message_surface, (WIDTH - 330, HEIGHT - 60))  
     pygame.display.flip()
-    time.sleep(1)
+    time.sleep(3)
 
 def draw_pv():
     """Fonction for showing the  PV"""
     pv_text1 = FONT.render(f"{pokemon1.get_name()}: {pokemon1.get_pv()} PV", True, BLACK)
     pv_text2 = FONT.render(f"{pokemon4.get_name()}: {pokemon4.get_pv()} PV", True, BLACK)
-    screen.blit(pv_text1, (500,50))
-    screen.blit(pv_text2, (500,100))
+    SCREEN.blit(pv_text1, (590,370))
+    SCREEN.blit(pv_text2, (50,65))
 
 def draw_message(victory_message, xp_message):
     """Fonction fot showing the victory/defeat message """
-    screen.fill(WHITE)
+    SCREEN.fill(WHITE)
     victory_surface = FONT.render(victory_message, True, BLACK)
     victory_rect = victory_surface.get_rect(center=(WIDTH//2, HEIGHT //2 - 20))
-    screen.blit(victory_surface, victory_rect)
+    SCREEN.blit(victory_surface, victory_rect)
     
     xp_surface = FONT.render(xp_message, True, BLACK)
     xp_rect = xp_surface.get_rect(center=(WIDTH //2, HEIGHT //2 + 20))
-    screen.blit(xp_surface, xp_rect)
+    SCREEN.blit(xp_surface, xp_rect)
 
     pygame.display.flip()
     time.sleep(3)
@@ -130,8 +151,8 @@ def draw_level_and_up():
     """Fonction for showing the level and XP"""
     level_xp_text1 = FONT.render(f"{pokemon1.get_name()}: Level {pokemon1.get_level()}, XP {pokemon1.get_xp()}/{pokemon1.get_xp_to_next_level()}", True, BLACK)
     level_xp_text2 = FONT.render(f"{pokemon4.get_name()}: Level {pokemon4.get_level()}, XP {pokemon4.get_xp()} / {pokemon4.get_xp_to_next_level()}", True, BLACK)
-    screen.blit(level_xp_text1, (500,150))
-    screen.blit(level_xp_text2, (500,200))
+    SCREEN.blit(level_xp_text1, (570,340))
+    SCREEN.blit(level_xp_text2, (30,35))
 
 clock = pygame.time.Clock()
 last_attack_time =0
@@ -168,18 +189,33 @@ while True:
                             draw_pv()
                             draw_level_and_up()
                             break
-                    elif button["text"] == "Change":
+                    elif button["text"] == "Catch":
                         print("changed my pokemon")
                     elif button["text"] == "Escape":
                         pygame.quit()
                         sys.exit()
 
-    screen.fill(WHITE)
-    pygame.draw.circle(screen, GREEN, [circle1_x, circle1_y], 50, 3)
-    pygame.draw.circle(screen, RED, [circle2_x, circle2_y], 50, 3)
-    draw_action_button()
+    SCREEN.blit(BACKGROUND, (0, 0))
+
+
+    SCREEN.blit(wild_pokemon, (a, b))
+    SCREEN.blit(bush1, (420, 140))
+    SCREEN.blit(bush2, (-150, 330))
+    SCREEN.blit(main_pokemon, (c, d))
+
+
+    SCREEN.blit(menu, (0, 420))
+
+
+    SCREEN.blit(hp1, (10,14))
+    SCREEN.blit(hp2, (520,320))
+    SCREEN.blit(wild_pokemon, (a, b))
     draw_pv()
     draw_level_and_up()
+    draw_action_button()
+
+
+
 
     pygame.display.flip()
 
